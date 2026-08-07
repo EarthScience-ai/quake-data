ecbad1d4c3cfd2cd187cef6742b8ad11f9e746e7
EarthScience-ai <sportyrocks56@gmail.com>
2026-08-07 22:49:22 +0900

    Update quake-inf.py

--- quake-inf.py
+++ quake-inf.py
@@ -3,19 +3,18 @@ import streamlit as st
 st.markdown(
     """
     <style>
-    .head{color: white; background-color: blue; padding: 10px;}
+    .head{
+         color: white; 
+         background-color: blue; 
+         padding: 10px;}
     </style>
     """, unsafe_allow_html=True
 )
-st.markdown('<h2 class="head">日本の地震国へようこそ</h2>', unsafe_allow_html=True)
-page = st.sidebar.selectbox('メニュー', ['震度データベース', '過去の地震、津波、及び火山災害', '防災知識を高めるには?'], index=0)
+page = st.sidebar.selectbox('メニュー', ['ホーム', '震度データベース', '過去の地震、津波、及び火山災害'], index=0)
 
-if page == '震度データベース':
-    st.title('ページ1')
-    st.write('Streamlitのテストページ1です。')
+if page == 'ホーム':
+    st.markdown('<h1 class="head">日本の地震国へようこそ</h1>', unsafe_allow_html=True)
+elif page == '震度データベース':
+    st.markdown('<h1 class="head">地震情報</h1>', unsafe_allow_html=True)
 elif page == '過去の地震、津波、及び火山災害':
-    st.title('ページ2')
-    st.write('Streamlitのテストページ2です。')
-else:
-    st.title('防災知識を高めるには?')
-    st.write('Streamlitのテストページ3です。')
+    st.markdown('<h1 class="head">過去の災害から防災や減災</h1>', unsafe_allow_html=True)
