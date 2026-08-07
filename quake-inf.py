9282fdda321aa9a5417282f90130ae7d85f77360
EarthScience-ai <sportyrocks56@gmail.com>
2026-08-07 22:22:50 +0900

    Update quake-inf.py

--- quake-inf.py
+++ quake-inf.py
@@ -1,14 +1,21 @@
 import streamlit as st
 
-st.header('Streamlitのテストページ', divider='blue')
-page = st.sidebar.selectbox('ページを選択', ['page1', 'page2', 'page3'], index=0)
+st.markdown(
+    """
+    <style>
+    .head{color: white; background-color: blue; padding: 10px;}
+    </style>
+    """, unsafe_allow_html=True
+)
+st.markdown('<h2 class="head">日本の地震国へようこそ</h2>')
+page = st.sidebar.selectbox('メニュー', ['震度データベース', '過去の地震、津波、及び火山災害', '防災知識を高めるには?'], index=0)
 
-if page == 'page1':
+if page == '震度データベース':
     st.title('ページ1')
     st.write('Streamlitのテストページ1です。')
-elif page == 'page2':
+elif page == '過去の地震、津波、及び火山災害':
     st.title('ページ2')
     st.write('Streamlitのテストページ2です。')
 else:
-    st.title('ページ3')
+    st.title('防災知識を高めるには?')
     st.write('Streamlitのテストページ3です。')
