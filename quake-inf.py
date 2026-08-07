243a408eed6c7523a18342b259849ed9b8e13823
EarthScience-ai <sportyrocks56@gmail.com>
2026-08-07 23:53:15 +0900

    Update quake-inf.py

--- quake-inf.py
+++ quake-inf.py
@@ -16,6 +16,11 @@ if page == 'ホーム':
     st.markdown('<p class="a">日本の地震国へようこそ</p>', unsafe_allow_html=True)
 elif page == '震度データベース':
     st.markdown('<p class="a">地震情報</p>', unsafe_allow_html=True)
-    st.sidebar.date_input("Date")
+    st.sidebar.date_input(
+        label="日付を選択",
+        value=datetime.date.today(), 
+        min_value=datetime.date(2001, 1, 1),
+        max_value=datetime.date(2100, 12, 31),
+    )
 elif page == '過去の地震、津波、及び火山災害':
     st.markdown('<p class="a">過去の災害から防災や減災</p>', unsafe_allow_html=True)
