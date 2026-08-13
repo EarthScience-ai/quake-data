f28812c25522c321897da996868798d91c8fb8af
EarthScience-ai <sportyrocks56@gmail.com>
2026-08-14 01:04:20 +0900

    Update quake-inf.py

--- quake-inf.py
+++ quake-inf.py
@@ -17,9 +17,9 @@ page = st.sidebar.radio('メニュー', ['HOME', '震度データベース', '�
 if page == 'HOME':
     st.markdown('<p class="a">ようこそ</p>', unsafe_allow_html=True)
 elif page == '震度データベース':
-    st.markdown('<p class="a">地震情報</p>', unsafe_allow_html=True)
+    st.markdown('<p class="a">過去の地震情報</p>', unsafe_allow_html=True)
     select_date = st.sidebar.date_input(
-        label="日付を選択",
+        label="地震発生日付を選択",
         value=datetime.date.today(), 
         min_value=datetime.date(2001, 1, 1),
         max_value=datetime.date(2100, 12, 31),
@@ -27,7 +27,7 @@ elif page == '震度データベース':
     df['datetime_date'] = pd.to_datetime(df['date'], format='%Y/%m/%d').dt.date
     filtered_df = df[df['datetime_date'] == select_date]
 elif page == '過去の地震、津波、及び火山災害':
-    st.markdown('<p class="a">過去大災害から防災や減災</p>', unsafe_allow_html=True)
+    st.markdown('<p class="a">過去の地震、津波、及び火山災害</p>', unsafe_allow_html=True)
 elif page == '想定南海トラフ巨大地震':
     st.markdown('<p class="a">内閣府　想定南海トラフ巨大地震</p>', unsafe_allow_html=True)
 elif page == '想定日本海溝・千島海溝巨大地震':
