a88515d202c68a6b201e44f98675de5f1a561d15
EarthScience-ai <sportyrocks56@gmail.com>
2026-08-23 23:25:11 +0900

    Update quake-inf.py

--- quake-inf.py
+++ quake-inf.py
@@ -65,7 +65,7 @@ elif page == '震度データベース':
                 elif shindo == "2": return '#00bfff'
                 elif shindo == "1": return '#0000ff'
             
-            m = folium.Map(location=[36.0, 137.0], zoom_start=5, tiles="OpenStreetMap")
+            m = folium.Map(location=[36.0, 137.0], zoom_start=5, tiles="CartoDB Voyager")
             
             for index, row in final_df.iterrows():
                 icon_x = folium.DivIcon(html=f'<div style="font-size: 16px; color: red; font-weight: bold; transform: translate(-50%, -50%);">❌</div>')
