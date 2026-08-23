4b72314fdc87726e288876c0027d17db3a4def18
EarthScience-ai <sportyrocks56@gmail.com>
2026-08-23 12:42:16 +0900

    Update quake-inf.py

--- quake-inf.py
+++ quake-inf.py
@@ -47,13 +47,11 @@ elif page == '震度データベース':
             st.sidebar.markdown(f"震源地: {epicenter_val}")
             st.sidebar.markdown(f"緯度: {epicenter_lat}　経度: {epicenter_lng}")
             st.sidebar.markdown(f"深さ: {depth_val}　M{magnitude_val}　　最大{max_intensity}")
-            st.subheader("地図表示")
             m = folium.Map(location=[36.0, 137.0], zoom_start=5)
             for index, row in final_df.iterrows():
-                popup_text = f"震源: {row['epicenter']}<br>規模: M{row['magnitude']} 深さ: {row['depth']}"
                 icon_x = folium.DivIcon(html=f'<div style="font-size: 16px; color: red; font-weight: bold; transform: translate(-50%, -50%);">❌</div>')
                 folium.Marker(location=[row["lat"], row["lng"]], popup=popup_text, icon=icon_x).add_to(m)
-            st_folium(m)
+            st_folium(m,width="100%")
     
 elif page == '過去の地震、津波、及び火山災害':
     st.markdown('<p class="a">過去の地震、津波、及び火山災害</p>', unsafe_allow_html=True)
